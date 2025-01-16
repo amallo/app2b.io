857516b653fb2b3848725e05797c12ce6e72407d
jpiquard <piquard.jonathan@protonmail.com>
2025-01-16 20:42:56 +0700

    fix: set header bg-color to white and add light bottom-border

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,7 +1,7 @@
 
 export const Header = () => {
     return (
-        <header className="fixed top-0 left-0 right-0 z-50 bg-orange-100 py-4 px-6">
+        <header className="fixed top-0 left-0 right-0 z-50 py-4 px-6 bg-white py-4 px-6 border-b border-b-black[.1]">
             <div className="flex flex-row items-center gap-2 max-w-7xl mx-auto">
                 <img src="logox128-bg-transparent.png" className="h-12 object-cover" />
                 <span className="text-2xl font-bold italic text-gray-600">APP2B</span>
